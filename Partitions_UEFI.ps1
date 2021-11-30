cdf135e897d0958a541a10511eb81910d22ac211
Filip Prytz <0207FIPR@gapp.uddevalla.se>
2021-11-30 09:03:22 +0100

    Hej

--- Partitions_UEFI.ps1
+++ Partitions_UEFI.ps1
@@ -1,3 +1,17 @@
+    [Cmdletbinding()]
+    Param()
+
+$iso = $PSScriptRoot + 'D:\iso\en_windows_server_2019_updated_jun_2021_x64_dvd_a2a2f782.iso' 
+$RefVhdxPath = $PSScriptRoot + '\Reference VHDX\ref2019.vhdx'
+
+Function New-RefVHDX {
+
+    [cmdletbinding()]
+    param(  
+         [string] $iso = $(throw 'no ISO specified'),
+         [string] $RefVhdxPath = $(throw 'no reference disk specified')
+         )
+
 # Variabler (ändra path)
 $ISO = 'D:\iso\en_windows_server_2019_updated_jun_2021_x64_dvd_a2a2f782.iso' 
 $RefVhdxPath = 'D:\Build\Ref2022.vhdx'
@@ -17,48 +31,37 @@ Write-Verbose "Created VHDX File [$($vmdisk01.path)]"
 Mount-DiskImage -ImagePath $RefVHDXPath
 $VHDDisk = Get-DiskImage -ImagePath $RefVHDXPath | Get-Disk
 $VHDDiskNumber = [string]$VHDDisk.Number
-Initialize-Disk -Number $VHDDiskNumber -PartitionStyle GPT
+# Initialize-Disk -Number $VHDDiskNumber -PartitionStyle GPT
 
 # Partitioner via Disk Part
 @"
-Select disk $VHDDiskNumber
-Convert GPT
-Create partition efi size=550
-Format FS=FAT32 quick label="System"
-Assign letter="S"
-Create partition MSR size=120
-Set id="e3c9e316-0b5c-4db8-817d-f92df00215ae" 
-GPT attributes=0x8000000000000001
-Create partition primary
-Shrink minimum=1024
-Format fs=ntfs quick label="Windows"
-Assign letter="W"
-Set id="ebd0a0a2-b9e5-4433-87c0-68b6b72699c7"
-GPT attributes=0x8000000000000001
-Create partition primary
-Format fs=ntfs quick label="Recovery"
-Assign letter="R"
-Set id="de94bba4-06d1-4d40-a16a-bfd50179d6ac"
-GPT attributes=0x8000000000000001
-List volume
-Exit
-"@ | diskpart.exe | Out-Null
-
-# MSR
-# New-Partition -DiskNumber $VHDDiskNumber `
-# -Size 120MB -GptType "{e3c9e316-0b5c-4db8-817d-f92df00215ae}" 
-
-# Datavolym
-# $VHDDrive = New-Partition -DiskNumber $VHDDiskNumber `
-# -AssignDriveLetter -UseMaximumSize -GptType "{ebd0a0a2-b9e5-4433-87c0-68b6b72699c7}" # Kör shrink
-
-# Recovery
-# New-Partition -DiskNumber $VHDDiskNumber `
-# -UseMaximumSize -GptType "{de94bba4-06d1-4d40-a16a-bfd50179d6ac}" |
+select disk $vhddisknumber
+convert gpt
+create partition efi size=1024
+format quick fs=fat32 label="System"
+gpt attributes=0x8000000000000001
+set id="c12a7328-f81f-11d2-ba4b-00a0c93ec93b"
+assign letter="S"
+create partition msr size=50
+gpt attributes=0x8000000000000001
+create partition primary 
+gpt attributes=0x8000000000000001
+shrink minimum=1024
+format quick fs=ntfs label="Data"
+assign letter="W"
+create partition primary
+format quick fs=ntfs label="Recovery"
+assign letter="R"
+set id="de94bba4-06d1-4d40-a16a-bfd50179d6ac"
+gpt attributes=0x8000000000000001
+list volume
+exit
+"@ | diskpart.exe
 
 # Formaterar volymen
 Format-Volume -Confirm:$false 
-$VHDVolume = "W:" # Formatera System, Datavolym och Recovery (använd pipes |)
+$VHDVolume = "W"+":"
+$SystemPartition = "S:"
 
 # Hämtar Windows versionerna
 $IndexList = Get-WindowsImage -ImagePath $ISODrive\sources\install.wim
@@ -75,13 +78,14 @@ Dism.exe /apply-Image /ImageFile:$ISODrive\Sources\install.wim /index:$Index /Ap
 Write-Verbose "Finished at [$(Get-Date)]"
 
 # Finishing touches, installerar firmware för UEFI
-BCDBoot.exe $VHDVolume\Windows /s $VHDVolume /f UEFI
-Dismount-DiskImage -ImagePath $ISO
-Dismount-DiskImage -ImagePath $RefVHDXPath
-Get-ChildItem $RefVHDXPath
+    BCDBoot.exe $VHDVolume\Windows /s $SystemPartition /f UEFI
+    Dismount-DiskImage -ImagePath $ISO
+    Dismount-DiskImage -ImagePath $RefVHDXPath
+    Get-ChildItem $RefVHDXPath
 
-# Skriver hur lång tid det tog för att skapa partitionen
-$FinishTime = Get-Date
-$TT = $FinishTime - $StartTime
-Write-Verbose  "Finishing at $FinishTime"
-Write-verbose  "Creating base image took [$($TT.totalminutes.tostring('n2'))] minutes"
+    # Skriver hur lång tid det tog för att skapa partitionen
+    $FinishTime = Get-Date
+    $TT = $FinishTime - $StartTime
+    Write-Verbose  "Finishing at $FinishTime"
+    Write-verbose  "Creating base image took [$($TT.totalminutes.tostring('n2'))] minutes"    
+}
